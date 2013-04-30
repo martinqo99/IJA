d337fcffd3d5eedf2ab2027301883376efabdafc
xkolac12 <xkolac12@stud.fit.vutbr.cz>
2013-04-30 22:02:46 +0200

    Provedena revize trid Figure a Position

--- Queen/src/queen/basis/Figure.java
+++ Queen/src/queen/basis/Figure.java
@@ -1,14 +1,19 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Projekt: Queen
+ * Predmet: IJA - Seminar Java
+ * Autori:
+ *          xkolac12 < xkolac12 @ stud.fit.vutbr.cz >
+ *          xmatya03 < xmatya03 @ stud.fit.vutbr.cz >
  */
+
 package queen.basis;
 
 import java.awt.*;
 
 /**
- *
- * @author xkolac12 <xkolac12 at stud.fit.vutbr.cz>
+ * @author      Frantisek Kolacek <xkolac12 @ stud.fit.vutbr.cz>
+ * @version     0.91
+ * @since       2013-04-30
  */
 abstract public class Figure {
     
@@ -16,26 +21,56 @@ abstract public class Figure {
     private Position position;
     private Color color;
     
+    /**
+     * Konstruktor pro objekt Figure
+     * @param position  pozice herniho pole
+     * @param color     barva herniho pole
+     */
     public Figure(Position position, Color color){
         this(null, position, color);
     }
     
+    /**
+     * Konstruktor pro objekt Figure
+     * @param desk      reference na hraci desku
+     * @param position  pozice herniho pole
+     * @param color     barva herniho pole
+     */
     public Figure(Desk desk, Position position, Color color){
         this.desk = desk;
         this.position = position;
         this.color = color;  
     }
     
-
-    
+    /**
+     * Getter pro ziskani pozice herniho pole
+     * @return  pozice herniho pole
+     */
     public Position getPosition(){
         return this.position;
     }
     
+    /**
+     * Getter pro ziskani barvy herniho pole
+     * @return  barva herniho pole
+     */
     public Color getColor(){
         return this.color;
     }
     
+    /**
+     * Getter pro ziskani reference na herni desku
+     * @return  reference na herni desku
+     */
+    public Desk getDesk(){
+        return this.desk;
+    }
+    
+    /**
+     * Metody pro premisteni figurky
+     * @param position  cilova pozice na herni desce
+     * @return          true pokud figurka muze pohyb uskutecnit, jinak false
+     */
     public boolean move(Position position){
         if(this.canMove(position)){
             this.position = position;
@@ -45,5 +80,10 @@ abstract public class Figure {
             return false;
     }
     
+    /**
+     * Abstraktni metoda pro kontrolu, zda figurka muze pohyb uskutecnit
+     * @param position  cilova pozice na herni desce
+     * @return          true pokud figurka muze pohyb uskutecnit, jinak false
+     */
     abstract public boolean canMove(Position position);    
 }
--- Queen/src/queen/basis/Position.java
+++ Queen/src/queen/basis/Position.java
@@ -1,29 +1,59 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Projekt: Queen
+ * Predmet: IJA - Seminar Java
+ * Autori:
+ *          xkolac12 < xkolac12 @ stud.fit.vutbr.cz >
+ *          xmatya03 < xmatya03 @ stud.fit.vutbr.cz >
  */
+
 package queen.basis;
 
 /**
- *
- * @author xkolac12 <xkolac12 at stud.fit.vutbr.cz>
+ * @author      Frantisek Kolacek <xkolac12 @ stud.fit.vutbr.cz>
+ * @version     0.91
+ * @since       2013-04-30
  */
 public class Position {
     
     private char column;
     private int row;
     
+    /**
+     * Konstruktor pro objekt Position
+     * @param column    znak sloupce
+     * @param row       cislo radku
+     */
     public Position(char column, int row){
         this.column = column;
         this.row = row;
     }
     
+    /**
+     * Getter pro znak sloupce
+     * @return  znak sloupce
+     */
     public char getColumn(){
         return this.column;
     }
     
+    /**
+     * Getter pro cislo radku
+     * @return cislo radku
+     */
     public int getRow(){
         return this.row;
+    }
+    
+    /**
+     * Porovnani dvou objektu Position
+     * @param position
+     * @return true pokud jsou shodne, jinak false
+     */
+    public boolean equals(Position position){
+        if(this.column == position.getColumn() && this.row == position.getRow())
+            return true;
+        else
+            return false;
     }
     
     @Override
