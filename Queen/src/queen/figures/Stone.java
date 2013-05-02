b2c5293dc3d6d2cfc04826916d184a957b34122e
KwisatzHaderach <xmatya03@stud.fit.vutbr.cz>
2013-05-02 10:42:07 +0200

    Upraveny sutr

--- Queen/src/queen/figures/Stone.java
+++ Queen/src/queen/figures/Stone.java
@@ -43,25 +43,29 @@ public class Stone extends Figure{
     @Override
     public Vector canMovePossibilities(){
         Vector possibilities = new Vector();
-        Vector kill = new Vector();
 
         int rowStep = (this.color == Color.BLACK)? -1 : 1;
 
         Position step1;
         Position step2;
+        Possibility possibility;
 
         // Vlevo
         step1 = new Position((char)(this.position.getColumn() - 1), this.position.getRow() + rowStep);
         if(!this.desk.isDeserter(step1)){
             // Volna pozice
-            if(this.desk.at(step1).getFigure() == null)
-                possibilities.add(new Position(step1.getColumn(), step1.getRow()));
+            if(this.desk.at(step1).getFigure() == null) {
+                possibility = new Possibility(step1);
+                possibilities.add(new Possibility(possibility));
+            }
             else if(this.color != this.desk.at(step1).getFigure().getColor()){
                 step2 = new Position((char)(step1.getColumn() - 1), step1.getRow() + rowStep);
 
-                if(!this.desk.isDeserter(step2) && this.desk.at(step2).getFigure() == null)
-                    kill.add(new Position(step1.getColumn(), step1.getRow()));
-                    possibilities.add(new Position(step2.getColumn(), step2.getRow()));
+                if(!this.desk.isDeserter(step2) && this.desk.at(step2).getFigure() == null) {
+                    possibility = new Possibility(step2);
+                    possibility.killVictim(step1);
+                    possibilities.add(new Possibility(possibility));
+                }
             }
         }
 
@@ -69,14 +73,23 @@ public class Stone extends Figure{
         step1 = new Position((char)(this.position.getColumn() + 1), this.position.getRow() + rowStep);
         if(!this.desk.isDeserter(step1)){
             // Volna pozice
-            if(this.desk.at(step1).getFigure() == null)
-                possibilities.add(new Position(step1.getColumn(), step1.getRow()));
+            if(this.desk.at(step1).getFigure() == null && possibilities.size() > 0 && ((Possibility)possibilities.firstElement()).killed() == 0)
+                possibilities.add(new Possibility(step1));
+            else if (this.desk.at(step1).getFigure() == null && possibilities.size() == 0)
+                possibilities.add(new Possibility(step1));
             else if(this.color != this.desk.at(step1).getFigure().getColor()){
                 step2 = new Position((char)(step1.getColumn() + 1), step1.getRow() + rowStep);
 
-                if(!this.desk.isDeserter(step2) && this.desk.at(step2).getFigure() == null)
-                    kill.add(new Position(step1.getColumn(), step1.getRow()));
-                    possibilities.add(new Position(step2.getColumn(), step2.getRow()));
+                if(!this.desk.isDeserter(step2) && this.desk.at(step2).getFigure() == null) {
+                    possibility = new Possibility(step2);
+                    possibility.killVictim(step1);
+                    if (possibilities.size() > 0 && ((Possibility)possibilities.firstElement()).killed() > 0)
+                        possibilities.add(new Possibility(possibility));
+                    else {
+                        possibilities = new Vector();
+                        possibilities.add(new Possibility(possibility));
+                    }
+                }
             }
         }
 
